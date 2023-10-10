7e1b05b21763df4493ae89a446fb9df72b3eae0a
Leandro F <leandro@licitegov.com.br>
2023-10-10 14:04:52 -0300

    feat: add state in login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import Button from "../../../shared/buttons/button/Button";
 import Input from "../../../shared/inputs/input/input";
 import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage, TitleLogin } from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value)
+    }
+    const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value)
+    }
+
+    const handleLogin = () => {
+        alert(`
+            ${username},
+            ${password}
+        `)
+    }
+
     return (<ContainerLoginScreen>
         <BackgroundImage src="./background.png" />
         <ContainerLogin>
             <LimitedContainer>
                 <LogoImage src="./logo.png" />
                 <TitleLogin level={2} type="secondary">Login</TitleLogin>
-                <Input title="Usuário"/>
-                <Input title="Senha" />
-                <Button type="primary" margin="64px 0 16px 0">ENTRAR</Button>
+                <Input title="Usuário" margin="32px 0 0 0" onChange={handleUsername}/>
+                <Input type="password" title="Senha" margin="32px 0 0 0" onChange={handlePassword} />
+                <Button type="primary" margin="64px 0 16px 0" onClick={handleLogin}>ENTRAR</Button>
             </LimitedContainer>
 
         </ContainerLogin>
--- src/shared/inputs/input/input.tsx
+++ src/shared/inputs/input/input.tsx
@@ -2,12 +2,13 @@ import { Input as InputAntd, InputProps as InputPropsAntd } from "antd";
 import { BoxInput, TitleInput } from "./input.styles";
 
 interface InputProps extends InputPropsAntd {
-    title?: string
+  title?: string,
+  margin?: string
 }
 
-const Input = ({title, ...props }: InputProps) => {
+const Input = ({title, margin, ...props }: InputProps) => {
   return (
-    <BoxInput>
+    <BoxInput style={{margin}}>
         {title && (
             <TitleInput>{title}</TitleInput>
         )}
