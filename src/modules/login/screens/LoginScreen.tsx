b4e6ae2634370ae6dfa59505c7aed19efb8ee47f
Leandro F <leandro@licitegov.com.br>
2023-10-25 12:22:05 -0300

    feat: insert global context

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
-import Button from "../../../shared/buttons/button/Button";
-import Input from "../../../shared/inputs/input/input";
+import { useContext, useState } from "react";
+import Button from "../../../shared/components/buttons/button/Button";
+import Input from "../../../shared/components/inputs/input/input";
 import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage, TitleLogin } from "../styles/loginScreen.styles";
 
 import axios from "axios";
-import SVGLogo from "../../../shared/icons/SVGLogo";
+import SVGLogo from "../../../shared/components/icons/SVGLogo";
+import { useRequests } from "../../../shared/hooks/useRequests";
+import { useGlobalContext } from "../../../shared/hooks/useGlobalContext";
 
 const LoginScreen = () => {
+    const { accessToken, setAccessToken } = useGlobalContext()
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const {postRequest, loading} = useRequests();
 
     const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(event.target.value)
@@ -17,35 +21,26 @@ const LoginScreen = () => {
         setPassword(event.target.value)
     }
 
-    const handleLogin = async () => {
-        const returnObject = await axios({
-            method: 'post',
-            url: "http://localhost:8080/auth",
-            data: {
-                email: email,
-                password: password
-            }
-        }).then((result) => {
-            alert(`Fez login! \n Email: ${email}, senha: ${password}`);
-
-            return result.data;
-        }).catch(()=>{
-            alert("Usuário inválido!")
-        });
-        console.log("returnObject", returnObject);
-        
+    const handleLogin = () => {
+        postRequest("http://localhost:8080/auth", {
+            email: email,
+            password: password
+        })
+        setAccessToken('Novo token')         
     }
 
     return (<ContainerLoginScreen>
         <BackgroundImage src="./background.png" />
         <ContainerLogin>
             <LimitedContainer>
-                <LogoImage src="./logo.png" />
                 <SVGLogo />
-                <TitleLogin level={2} type="secondary">Login</TitleLogin>
+                <TitleLogin level={2} type="secondary">
+                    Login
+                    ({accessToken})
+                </TitleLogin>
                 <Input title="Usuário" margin="32px 0 0 0" onChange={handleEmail}/>
                 <Input type="password" title="Senha" margin="32px 0 0 0" onChange={handlePassword} />
-                <Button type="primary" margin="64px 0 16px 0" onClick={handleLogin}>ENTRAR</Button>
+                <Button type="primary" loading={loading} margin="64px 0 16px 0" onClick={handleLogin}>ENTRAR</Button>
             </LimitedContainer>
 
         </ContainerLogin>
